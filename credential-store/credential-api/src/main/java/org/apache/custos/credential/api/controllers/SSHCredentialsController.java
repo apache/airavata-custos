e4021296257df31e5c8a56e8a7ab821f5800a10b
Dimuthu Wannipurage <dimuthu.upeksha2@gmail.com>
2019-07-09 02:12:13 -0400

    API to add SSH credentials

--- credential-store/credential-api/src/main/java/org/apache/custos/credential/api/controllers/SSHCredentialsController.java
+++ credential-store/credential-api/src/main/java/org/apache/custos/credential/api/controllers/SSHCredentialsController.java
@@ -5,10 +5,7 @@ import org.apache.airavata.custos.vault.VaultManager;
 import org.apache.custos.credential.api.resources.SSHCredential;
 import org.dozer.DozerBeanMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SSHCredentialsController {
@@ -23,5 +20,12 @@ public class SSHCredentialsController {
     public SSHCredential getSSHCredential(@PathVariable("gateway") String gateway, @PathVariable("token") String token) throws Exception {
         SSHCredentialEntity credentialEntity = vaultManager.getCredentialEntity(SSHCredentialEntity.class, token, gateway);
         return mapper.map(credentialEntity, SSHCredential.class);
+    }
+
+    @RequestMapping(value = "/ssh/{gateway}", method = RequestMethod.POST)
+    public String createSSHCredential(@RequestBody SSHCredential sshCredential, @PathVariable("gateway") String gateway) throws Exception {
+        SSHCredentialEntity credentialEntity = mapper.map(sshCredential, SSHCredentialEntity.class);
+        String token = vaultManager.saveCredentialEntity(credentialEntity, gateway);
+        return token;
     }
 }
