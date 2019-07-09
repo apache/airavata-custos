1af4ad183c66596ee73428a98b5fcf1663734305
Dimuthu Wannipurage <dimuthu.upeksha2@gmail.com>
2019-07-09 01:48:59 -0400

    Dozer mapping to map between credential entity and resources

--- credential-store/credential-api/src/main/java/org/apache/custos/credential/api/controllers/SSHCredentialsController.java
+++ credential-store/credential-api/src/main/java/org/apache/custos/credential/api/controllers/SSHCredentialsController.java
@@ -3,10 +3,11 @@ package org.apache.custos.credential.api.controllers;
 import org.apache.airavata.custos.credentials.ssh.SSHCredentialEntity;
 import org.apache.airavata.custos.vault.VaultManager;
 import org.apache.custos.credential.api.resources.SSHCredential;
+import org.dozer.DozerBeanMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,14 +16,11 @@ public class SSHCredentialsController {
     @Autowired
     private VaultManager vaultManager;
 
+    final private DozerBeanMapper mapper = new DozerBeanMapper();
 
-    @RequestMapping("/ssh/{gateway}/{token}")
+    @RequestMapping(value = "/ssh/{gateway}/{token}", method = RequestMethod.GET)
     public SSHCredential getSSHCredential(@PathVariable("gateway") String gateway, @PathVariable("token") String token) throws Exception {
         SSHCredentialEntity credentialEntity = vaultManager.getCredentialEntity(SSHCredentialEntity.class, token, gateway);
-        SSHCredential resource = new SSHCredential();
-        resource.setPassphrase(credentialEntity.getPassphrase());
-        resource.setPrivateKey(credentialEntity.getPrivateKey());
-        resource.setPublicKey(credentialEntity.getPublicKey());
-        return resource;
+        return mapper.map(credentialEntity, SSHCredential.class);
     }
 }
