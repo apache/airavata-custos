2e10e1324e126dffc1684ff870693c099018d85f
Aarushi <aarushiibisht@gmail.com>
2019-10-17 13:24:13 -0400

    refactored profile SDK

--- clients/python/airavata_custos/profile/profile.py
+++ clients/python/airavata_custos/profile/profile.py
@@ -1,63 +1,5 @@
 from airavata_custos import utils
-
-
-class Profile(object):
-
-    def __init__(self):
-        host = ""
-        port = ""
-        self.profile_service_pool = utils.initialize_userprofile_client_pool(host, port)
-
-    def create_user(self, authorization_token, user_profile, tenant):
-        """
-        This method creates a new user in custos profile service
-        :param authorization_token: object of class AuthzToken
-        :param user_profile: object of class User
-        :param tenant identifier in which new user will be created
-        :return: boolean true if user is created successfully otherwise false
-        """
-        return self.profile_service_pool.addUserProfile(authorization_token, user_profile, tenant)
-
-    def update_user(self, authorization_token, user_profile, tenant):
-        """
-        This method updates the user in custos profile service
-        :param authorization_token: object of class AuthzToken
-        :param user_profile: updated user info
-        :param tenant: tenant identifier to which the user belongs
-        :return: boolean true if user is updated successfully otherwise false
-        """
-        return self.updateUserProfile(authorization_token, user_profile, tenant)
-
-    def delete_user(self, authorization_token, user_name, tenant):
-        """
-        This method deletes the user in the custos profile service
-        :param authorization_token: object of class AuthzToken
-        :param user_name: unique identifier of the user
-        :param tenant: tenant identifier to which the user belongs
-        :return: boolean true if user is deleted successfully otherwise false
-        """
-        return self.deleteUserProfile(authorization_token, user_name, tenant)
-
-    def get_user(self, authorization_token, user_name, tenant):
-        """
-        To retrieve user info
-        :param authorization_token:  object of class AuthzToken
-        :param user_name: unique identifier of the user
-        :param tenant: tenant identifier to which the user belongs
-        :return: object of class User
-        """
-        return self.profile_service_pool.getUserProfileById(authorization_token, user_name, tenant)
-
-    def get_all_users(self, authorization_token, tenant, offset=0, limit=-1):
-        """
-        To retrieve all users in the tenant
-        :param authorization_token: object of class AuthzToken
-        :param tenant: tenant identifier
-        :param offset: to limit the number of users
-        :param limit: to limit the number of users
-        :return: list of users
-        """
-        return self.profile_service_pool.getAllUserProfilesInGateway(authorization_token, tenant, offset, limit)
+from airavata_custos.security.keycloak_connectors import AuthzToken
 
 
 class User(object):
@@ -94,3 +36,65 @@ class User(object):
         self.nationality = nationality
         self.labeled_URI = labeled_URI
         self.time_zone = time_zone
+
+
+class Profile(object):
+
+    def __init__(self):
+        host = ""
+        port = ""
+        self.profile_service_pool = utils.initialize_userprofile_client_pool(host, port)
+
+    def create_user(self, authorization_token: AuthzToken, user_profile: User = None) -> User:
+        """
+        This method creates a new user in custos profile service
+        :param authorization_token: object of class AuthzToken
+        :param user_profile: object of class User
+        :return: boolean true if user is created successfully otherwise false
+        """
+        if user_profile:
+            return self.profile_service_pool.addUserProfile(authorization_token, user_profile, user_profile.tenant)
+        else:
+            return self.profile_service_pool.initializeUserProfile(authorization_token)
+
+    def update_user(self, authorization_token, user_profile):
+        """
+        This method updates the user in custos profile service
+        :param authorization_token: object of class AuthzToken
+        :param user_profile: updated user info
+        :param tenant: tenant identifier to which the user belongs
+        :return: boolean true if user is updated successfully otherwise false
+        """
+        return self.updateUserProfile(authorization_token, user_profile)
+
+    def delete_user(self, authorization_token, user_name, tenant):
+        """
+        This method deletes the user in the custos profile service
+        :param authorization_token: object of class AuthzToken
+        :param user_name: unique identifier of the user
+        :param tenant: tenant identifier to which the user belongs
+        :return: boolean true if user is deleted successfully otherwise false
+        """
+        return self.deleteUserProfile(authorization_token, user_name, tenant)
+
+    def get_user(self, authorization_token, user_name, tenant):
+        """
+        To retrieve user info
+        :param authorization_token:  object of class AuthzToken
+        :param user_name: unique identifier of the user
+        :param tenant: tenant identifier to which the user belongs
+        :return: object of class User
+        """
+        return self.profile_service_pool.getUserProfileById(authorization_token, user_name, tenant)
+
+    def get_all_users(self, authorization_token, tenant, offset=0, limit=-1):
+        """
+        To retrieve all users in the tenant
+        :param authorization_token: object of class AuthzToken
+        :param tenant: tenant identifier
+        :param offset: to limit the number of users
+        :param limit: to limit the number of users
+        :return: list of users
+        """
+        return self.profile_service_pool.getAllUserProfilesInGateway(authorization_token, tenant, offset, limit)
+
