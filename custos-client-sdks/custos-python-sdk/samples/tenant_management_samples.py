6e8cb2b8b24deb896b72413aa3a1c3481f63415a
Isuru Ranawaka <irjanith@gmail.com>
2020-04-17 15:02:32 -0400

    remote token

--- custos-client-sdks/custos-python-sdk/samples/tenant_management_samples.py
+++ custos-client-sdks/custos-python-sdk/samples/tenant_management_samples.py
@@ -28,7 +28,7 @@ handler.setLevel(logging.DEBUG)
 client = TenantManagementClient()
 admin_client = SuperTenantManagementClient()
 
-token = "Y3VzdG9zLTZud29xb2RzdHBlNW12Y3EwOWxoLTEwMDAwMTAxOkdpS3JHR1ZMVzd6RG9QWnd6Z0NpRk03V1V6M1BoSXVtVG1GeEFrcjc="
+token = "XXX"
 
 
 def create_tenant():
