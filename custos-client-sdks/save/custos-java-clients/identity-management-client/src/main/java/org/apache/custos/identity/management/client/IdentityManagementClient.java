1fd7a452e31bb78d8d0783501d65f17f4ec17ad0
Isuru Ranawaka <irjanith@gmail.com>
2020-05-05 18:02:24 -0400

    save

---
+++ custos-client-sdks/save/custos-java-clients/identity-management-client/src/main/java/org/apache/custos/identity/management/client/IdentityManagementClient.java
@@ -0,0 +1,173 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.apache.custos.identity.management.client;
+
+import com.google.protobuf.Struct;
+import io.grpc.ManagedChannel;
+import io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.NettyChannelBuilder;
+import io.grpc.stub.MetadataUtils;
+import org.apache.custos.clients.core.ClientUtils;
+import org.apache.custos.credential.store.service.Credentials;
+import org.apache.custos.identity.management.service.GetAgentTokenRequest;
+import org.apache.custos.identity.management.service.GetCredentialsRequest;
+import org.apache.custos.identity.management.service.IdentityManagementServiceGrpc;
+import org.apache.custos.identity.service.EndSessionRequest;
+import org.apache.custos.identity.service.GetOIDCConfiguration;
+import org.apache.custos.identity.service.GetTokenRequest;
+import org.apache.custos.identity.service.OperationStatus;
+
+import java.io.IOException;
+
+/**
+ * Java client to connect with the Custos Identity Management Service
+ */
+public class IdentityManagementClient {
+
+
+    private ManagedChannel managedChannel;
+
+    private IdentityManagementServiceGrpc.IdentityManagementServiceBlockingStub blockingStub;
+
+
+    public IdentityManagementClient(String serviceHost, int servicePort, String clientId,
+                                    String clientSecret) throws IOException {
+
+        managedChannel = NettyChannelBuilder.forAddress(serviceHost, servicePort)
+                .sslContext(GrpcSslContexts
+                        .forClient()
+                        .trustManager(ClientUtils.getServerCertificate(serviceHost, clientId, clientSecret)) // public key
+                        .build())
+                .build();
+
+        blockingStub = IdentityManagementServiceGrpc.newBlockingStub(managedChannel);
+        blockingStub = MetadataUtils.attachHeaders(blockingStub, ClientUtils.getAuthorizationHeader(clientId, clientSecret));
+    }
+
+
+    /**
+     * Get token
+     *
+     * @param redirectUri
+     * @param code
+     * @param username
+     * @param password
+     * @param refreshToken
+     * @param grantType
+     * @return
+     */
+    public Struct getToken(String redirectUri, String code, String username, String password, String refreshToken,
+                           String grantType) {
+
+        GetTokenRequest.Builder request = GetTokenRequest.newBuilder();
+
+        request = request
+                .setRedirectUri(redirectUri)
+                .setCode(code);
+
+        if (username != null) {
+            request = request.setUsername(username);
+        }
+        if (password != null) {
+            request = request.setPassword(password);
+        }
+        if (refreshToken != null) {
+            request = request.setRefreshToken(refreshToken);
+        }
+        if (grantType != null) {
+            request = request.setGrantType(grantType);
+        }
+
+        return blockingStub.token(request.build());
+
+    }
+
+
+    /**
+     * Get iam, cilogon credentials of given tenant
+     *
+     * @param clientId
+     * @return
+     */
+    public Credentials getCredentials(String clientId) {
+        GetCredentialsRequest request = GetCredentialsRequest.newBuilder().setClientId(clientId).build();
+        return blockingStub.getCredentials(request);
+    }
+
+
+    /**
+     * Get OIDC configurations of given client
+     * @param clientId
+     * @return
+     */
+    public Struct getOIDCConfiguration(String clientId) {
+
+        GetOIDCConfiguration configuration = GetOIDCConfiguration
+                .newBuilder()
+                .setClientId(clientId).build();
+        return blockingStub.getOIDCConfiguration(configuration);
+
+    }
+
+
+    /**
+     * End user session
+     * @param refreshToken
+     * @return
+     */
+    public OperationStatus endUserSession(String refreshToken) {
+        EndSessionRequest body = EndSessionRequest
+                .newBuilder()
+                .setRefreshToken(refreshToken)
+                .build();
+        org.apache.custos.identity.management.service.EndSessionRequest endSessionRequest =
+                org.apache.custos.identity.management.service.EndSessionRequest
+                        .newBuilder()
+                        .setBody(body)
+                        .build();
+        return blockingStub.endUserSession(endSessionRequest);
+    }
+
+
+    /**
+     * Get agent tokens
+     * @param clientId
+     * @param grantType
+     * @param refreshToken
+     * @return
+     */
+    public Struct getAgentToken(String clientId, String grantType, String refreshToken) {
+
+        GetAgentTokenRequest.Builder agentTokenRequest = GetAgentTokenRequest
+                .newBuilder()
+                .setClientId(clientId)
+                .setGrantType(grantType);
+
+        if (refreshToken != null) {
+            agentTokenRequest = agentTokenRequest.setRefreshToken(refreshToken);
+        }
+
+
+        return blockingStub.getAgentToken(agentTokenRequest.build());
+
+    }
+
+
+}
---
+++ custos-client-sdks/save/custos-java-clients/user-management-client/src/main/java/UserManagementClient.java
@@ -0,0 +1,269 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+
+import io.grpc.ManagedChannel;
+import io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.NettyChannelBuilder;
+import io.grpc.stub.MetadataUtils;
+import org.apache.custos.clients.core.ClientUtils;
+import org.apache.custos.iam.service.*;
+import org.apache.custos.user.management.service.UserManagementServiceGrpc;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * The class containes operations permitted for user management client
+ */
+public class UserManagementClient {
+
+    private ManagedChannel managedChannel;
+
+    private UserManagementServiceGrpc.UserManagementServiceBlockingStub blockingStub;
+
+
+    public UserManagementClient(String serviceHost, int servicePort, String clientId,
+                                String clientSecret) throws IOException {
+
+        managedChannel = NettyChannelBuilder.forAddress(serviceHost, servicePort)
+                .sslContext(GrpcSslContexts
+                        .forClient()
+                        .trustManager(ClientUtils.getServerCertificate(serviceHost, clientId, clientSecret)) // public key
+                        .build())
+                .build();
+
+        blockingStub = UserManagementServiceGrpc.newBlockingStub(managedChannel);
+
+        blockingStub = MetadataUtils.attachHeaders(blockingStub,
+                ClientUtils.getAuthorizationHeader(clientId, clientSecret));
+
+    }
+
+    public RegisterUserResponse registerUser(String username, String firstName, String lastName,
+                                             String password, String email, boolean isTempPassword) {
+
+        UserRepresentation userRepresentation = UserRepresentation
+                .newBuilder()
+                .setUsername(username)
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setPassword(password)
+                .setEmail(email)
+                .setTemporaryPassword(isTempPassword)
+                .build();
+
+        RegisterUserRequest registerUserRequest = RegisterUserRequest
+                .newBuilder()
+                .setUser(userRepresentation)
+                .build();
+
+        return blockingStub.registerUser(registerUserRequest);
+
+    }
+
+
+    public UserRepresentation enableUser(String userName) {
+
+        UserSearchMetadata metadata = UserSearchMetadata
+                .newBuilder()
+                .setUsername(userName)
+                .build();
+
+
+        UserSearchRequest request = UserSearchRequest
+                .newBuilder()
+                .setUser(metadata).build();
+
+        return blockingStub.enableUser(request);
+    }
+
+    public OperationStatus addUserAttributes(String adminToken, UserAttribute[] attributes, String users) {
+
+        UserManagementServiceGrpc.UserManagementServiceBlockingStub blockingStub =
+                MetadataUtils.attachHeaders(this.blockingStub, ClientUtils.getAuthorizationHeader(adminToken));
+
+        AddUserAttributesRequest request = AddUserAttributesRequest
+                .newBuilder()
+                .addAllAttributes(Arrays.asList(attributes))
+                .addAllUsers(Arrays.asList(users))
+                .build();
+        return blockingStub.addUserAttributes(request);
+
+    }
+
+    public OperationStatus deleteUserAttributes(String adminToken, UserAttribute[] attributes, String users) {
+
+        UserManagementServiceGrpc.UserManagementServiceBlockingStub blockingStub =
+                MetadataUtils.attachHeaders(this.blockingStub, ClientUtils.getAuthorizationHeader(adminToken));
+
+
+        DeleteUserAttributeRequest request = DeleteUserAttributeRequest
+                .newBuilder()
+                .addAllAttributes(Arrays.asList(attributes))
+                .addAllUsers(Arrays.asList(users))
+                .build();
+        return blockingStub.deleteUserAttributes(request);
+
+    }
+
+
+    public OperationStatus addRolesToUsers(String adminToken, String[] roles, String[] username, boolean isClientLevel) {
+        UserManagementServiceGrpc.UserManagementServiceBlockingStub blockingStub =
+                MetadataUtils.attachHeaders(this.blockingStub, ClientUtils.getAuthorizationHeader(adminToken));
+
+        AddUserRolesRequest request = AddUserRolesRequest
+                .newBuilder()
+                .addAllRoles(Arrays.asList(roles))
+                .addAllUsernames(Arrays.asList(username))
+                .setClientLevel(isClientLevel)
+                .build();
+        return blockingStub.addRolesToUsers(request);
+
+    }
+
+
+    public OperationStatus deleteUserRoles(String adminToken, String[] clientRoles, String[] realmRoles, String username) {
+        UserManagementServiceGrpc.UserManagementServiceBlockingStub blockingStub =
+                MetadataUtils.attachHeaders(this.blockingStub, ClientUtils.getAuthorizationHeader(adminToken));
+
+        DeleteUserRolesRequest request = DeleteUserRolesRequest
+                .newBuilder()
+                .addAllClientRoles(Arrays.asList(clientRoles))
+                .addAllRoles(Arrays.asList(realmRoles))
+                .setUsername(username)
+                .build();
+        return blockingStub.deleteUserRoles(request);
+
+    }
+
+
+    public OperationStatus isUserEnabled(String username) {
+
+        UserSearchMetadata metadata = UserSearchMetadata
+                .newBuilder()
+                .setUsername(username)
+                .build();
+
+        UserSearchRequest request = UserSearchRequest
+                .newBuilder()
+                .setUser(metadata)
+                .build();
+
+        return blockingStub.isUserEnabled(request);
+    }
+
+
+    public OperationStatus isUsernameAvailable(String username) {
+
+        UserSearchMetadata metadata = UserSearchMetadata
+                .newBuilder()
+                .setUsername(username)
+                .build();
+
+        UserSearchRequest request = UserSearchRequest
+                .newBuilder()
+                .setUser(metadata)
+                .build();
+
+        return blockingStub.isUsernameAvailable(request);
+    }
+
+
+    public UserRepresentation getUser(String username) {
+        UserSearchMetadata metadata = UserSearchMetadata
+                .newBuilder()
+                .setUsername(username)
+                .build();
+
+        UserSearchRequest request = UserSearchRequest
+                .newBuilder()
+                .setUser(metadata)
+                .build();
+
+        return blockingStub.getUser(request);
+
+    }
+
+
+    public FindUsersResponse findUser(String username, String firstName, String lastName, String email, int offset, int limit) {
+
+        UserSearchMetadata.Builder builder = UserSearchMetadata
+                .newBuilder();
+
+        if (username != null) {
+            builder = builder.setUsername(username);
+        }
+
+        if (firstName != null) {
+            builder = builder.setFirstName(firstName);
+        }
+
+        if (lastName != null) {
+            builder = builder.setFirstName(lastName);
+        }
+
+        if (email != null) {
+            builder = builder.setFirstName(email);
+        }
+        UserSearchMetadata metadata = builder.build();
+
+        FindUsersRequest request = FindUsersRequest
+                .newBuilder()
+                .setUser(metadata)
+                .setLimit(limit)
+                .setOffset(offset)
+                .build();
+
+        return blockingStub.findUsers(request);
+
+    }
+
+
+    public OperationStatus resetUserPassword(String username, String password) {
+
+        ResetUserPassword userPassword = ResetUserPassword
+                .newBuilder()
+                .setPassword(password)
+                .setUsername(username)
+                .build();
+
+        return blockingStub.resetPassword(userPassword);
+
+    }
+
+
+    public OperationStatus deleteUser(String username) {
+
+        UserSearchMetadata metadata = UserSearchMetadata
+                .newBuilder()
+                .setUsername(username)
+                .build();
+
+        UserSearchRequest request = UserSearchRequest
+                .newBuilder()
+                .setUser(metadata)
+                .build();
+
+        return blockingStub.deleteUser(request);
+
+    }
+
+
+}
