a16b8a6242fafdd7b4ffdf4768daf25e2f1047c8
Isuru Ranawaka <irjanith@gmail.com>
2020-07-21 11:32:20 -0400

    Bug fix in settings.py

--- custos-client-sdks/custos-python-sdk/custos/transport/settings.py
+++ custos-client-sdks/custos-python-sdk/custos/transport/settings.py
@@ -15,17 +15,22 @@ class CustosServerClientSettings(object):
                  custos_port=None, custos_client_id=None, custos_client_sec=None):
         if configuration_file_location is not None:
             config.read(configuration_file_location)
-        self.CUSTOS_SERVER_HOST = config.get('CustosServer', 'SERVER_HOST')
-        self.CUSTOS_SERVER_PORT = config.getint('CustosServer', 'SERVER_SSL_PORT')
         self.CUSTOS_CERT_PATH = cert_path
-        self.CUSTOS_CLIENT_ID = config.get('CustosServer', 'CLIENT_ID')
-        self.CUSTOS_CLIENT_SEC = config.get('CustosServer', 'CLIENT_SEC')
 
         if custos_host is not None:
             self.CUSTOS_SERVER_HOST = custos_host
+        else:
+            self.CUSTOS_SERVER_HOST = config.get('CustosServer', 'SERVER_HOST')
         if custos_port is not None:
             self.CUSTOS_SERVER_PORT = custos_port
+        else:
+            self.CUSTOS_SERVER_PORT = config.getint('CustosServer', 'SERVER_SSL_PORT')
         if custos_client_id is not None:
             self.CUSTOS_CLIENT_ID = custos_client_id
+        else:
+            self.CUSTOS_CLIENT_ID = config.get('CustosServer', 'CLIENT_ID')
+
         if custos_client_sec is not None:
             self.CUSTOS_CLIENT_SEC = custos_client_sec
+        else:
+            self.CUSTOS_CLIENT_SEC = config.get('CustosServer', 'CLIENT_SEC')
