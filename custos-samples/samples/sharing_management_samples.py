0dcbb1c1252fd669ce9b44f050358deca900017a
Isuru Ranawaka <irjanith@gmail.com>
2020-10-04 09:49:49 -0400

    custos-demo

---
+++ custos-samples/samples/sharing_management_samples.py
@@ -0,0 +1,89 @@
+import os
+
+from custos.clients.sharing_management_client import SharingManagementClient
+
+from custos.transport.settings import CustosServerClientSettings
+import custos.clients.utils.utilities as utl
+
+# load root directoty
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# get settings file path (settings file path reside in configs folder under home directory)
+settings_path = os.path.join(BASE_DIR, 'configs', "settings.ini")
+
+# read settings
+custos_settings = CustosServerClientSettings(configuration_file_location=settings_path)
+
+# create custos user management client
+sharing_management_client = SharingManagementClient(custos_settings)
+
+# obtain base 64 encoded token for tenant
+b64_encoded_custos_token = utl.get_token(custos_settings=custos_settings)
+
+
+def create_permission_type(id, name, description):
+    response = sharing_management_client.create_permission_type(token=b64_encoded_custos_token,
+                                                                client_id=custos_settings.CUSTOS_CLIENT_ID,
+                                                                id=id,
+                                                                name=name,
+                                                                description=description)
+    print(response)
+
+
+def create_entity_type(id, name, description):
+    response = sharing_management_client.create_entity_type(token=b64_encoded_custos_token,
+                                                            client_id=custos_settings.CUSTOS_CLIENT_ID,
+                                                            id=id,
+                                                            name=name,
+                                                            description=description)
+    print(response)
+
+
+def create_entity(id, name, description, owner_id, type):
+    response = sharing_management_client.create_entity(token=b64_encoded_custos_token,
+                                                       client_id=custos_settings.CUSTOS_CLIENT_ID,
+                                                       id=id,
+                                                       name=name,
+                                                       description=description,
+                                                       owner_id=owner_id,
+                                                       type=type,
+                                                       parent_id='')
+    print(response)
+
+
+def share_entity_with_user(entity_id, permission_type, user_id):
+    response = sharing_management_client.share_entity_with_users(token=b64_encoded_custos_token,
+                                                                 client_id=custos_settings.CUSTOS_CLIENT_ID,
+                                                                 entity_id=entity_id,
+                                                                 permission_type=permission_type,
+                                                                 user_id=user_id)
+    print(response)
+
+
+def share_entity_with_group(entity_id, permission_type, group_id):
+    response = sharing_management_client.share_entity_with_groups(token=b64_encoded_custos_token,
+                                                                  client_id=custos_settings.CUSTOS_CLIENT_ID,
+                                                                  entity_id=entity_id,
+                                                                  permission_type=permission_type,
+                                                                  group_id=group_id)
+    print(response)
+
+
+def check_user_has_access(entity_id, permission_type, user_id):
+    response = sharing_management_client.user_has_access(token=b64_encoded_custos_token,
+                                                         client_id=custos_settings.CUSTOS_CLIENT_ID,
+                                                         entity_id=entity_id,
+                                                         permission_type=permission_type,
+                                                         user_id=user_id)
+    print(response)
+
+
+# create_permission_type('RW', 'Read and Write', 'Read write permissions')
+# create_entity_type("CRED_TOKEN", "Credential Token", "This is credential token")
+
+create_entity('655e8845-9afa-4251-bb0e-c1ebasasx42bec2fcASD', 'Password Token', 'This is password token', 'TestUser5',
+              'CRED_TOKEN')
+# share_entity_with_user('655e8845-9afa-4251-bb0e-c1eb42bec2fc', 'RW', 'TestUser4')
+# share_entity_with_group('655e8845-9afa-4251-bb0e-c1eb42bec2fc', 'RW', '602336d5-e193-41ac-bde6-eb36a73f687e')
+#
+# check_user_has_access('655e8845-9afa-4251-bb0e-c1eb42bec2fc', 'RW', 'TestUser4')
