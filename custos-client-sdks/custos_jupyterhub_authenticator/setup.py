4243a96b72ed97fe226f9b231eddadf7938f3aa8
Isuru Ranawaka <irjanith@gmail.com>
2022-02-12 08:23:55 -0500

    Add readme

--- custos-client-sdks/custos_jupyterhub_authenticator/setup.py
+++ custos-client-sdks/custos_jupyterhub_authenticator/setup.py
@@ -10,14 +10,16 @@ def read(fname):
 
 setup(
     name='custos_jupyterhub_authenticator',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     package_data={'': ['*.pem']},
     include_package_data=True,
-    url='https://airavata.apache.org/custos/',
+    url='https://github.com/apache/airavata-custos/tree/develop/custos-client-sdks/custos_jupyterhub_authenticator',
     license='Apache License 2.0',
     author='Custos Developers',
     author_email='dev@airavata.apache.org',
     install_requires=['oauthenticator>=14.2.0'],
-    description='Apache Custos Jupyterhub  Authenticator'
+    description='Apache Custos Jupyterhub  Authenticator',
+    long_description='Custos authenticator provides CILogon based federated authentication for Jupyterhub. In addition it provides '
+                     'Identity and Access Management solutions such as Fine-Grained Authorization, Secret Management ..etc',
 )
