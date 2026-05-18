26006aa515011210e4b9c16d0442b284c2feeb68
DImuthuUpe <dimuthu.upeksha2@gmail.com>
2026-05-18 08:56:52 -0400

    Fixing minor misconfiguration

--- connectors/SLURM/Association-Mapper/internal/subscribers/account.go
+++ connectors/SLURM/Association-Mapper/internal/subscribers/account.go
@@ -10,33 +10,33 @@ import (
 	"github.com/apache/airavata-custos/pkg/models"
 )
 
-func (a *AssociationSubscriber) SubscribeToComputeAccountCreation(computeAccount models.ComputeAllocation) {
-	slog.Info("Received compute account creation event", "account", computeAccount)
+func (a *AssociationSubscriber) SubscribeToComputeAllocationCreation(computeAccount models.ComputeAllocation) {
+	slog.Info("Received compute allocation creation event", "account", computeAccount)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	cluster, err := a.coreService.GetComputeCluster(ctx, computeAccount.ComputeClusterID)
 	if err != nil {
-		slog.Error("Failed to get compute cluster for account creation", "error", err)
+		slog.Error("Failed to get compute cluster for allocation creation", "error", err)
 		return
 	}
 
 	project, err := a.coreService.GetProject(ctx, computeAccount.ProjectID)
 	if err != nil {
-		slog.Error("Failed to get project for account creation", "error", err)
+		slog.Error("Failed to get project for allocation creation", "error", err)
 		return
 	}
 
 	pi, err := a.coreService.GetUser(ctx, project.ProjectPIID)
 	if err != nil {
-		slog.Error("Failed to get project PI for account creation", "error", err)
+		slog.Error("Failed to get project PI for allocation creation", "error", err)
 		return
 	}
 
 	organization, err := a.coreService.GetOrganization(ctx, pi.OrganizationID)
 	if err != nil {
-		slog.Error("Failed to get organization for account creation", "error", err)
+		slog.Error("Failed to get organization for allocation creation", "error", err)
 		return
 	}
 
@@ -52,10 +52,10 @@ func (a *AssociationSubscriber) SubscribeToComputeAccountCreation(computeAccount
 	}
 }
 
-func (a *AssociationSubscriber) SubscribeToComputeAccountDeletion(computeAccount models.ComputeAllocation) {
-	slog.Info("Received compute account deletion event", "account", computeAccount)
+func (a *AssociationSubscriber) SubscribeToComputeAllocationDeletion(computeAccount models.ComputeAllocation) {
+	slog.Info("Received compute allocation deletion event", "account", computeAccount)
 }
 
-func (a *AssociationSubscriber) SubscribeToComputeAccountUpdate(computeAccount models.ComputeAllocation) {
-	slog.Info("Received compute account update event", "account", computeAccount)
+func (a *AssociationSubscriber) SubscribeToComputeAllocationUpdate(computeAccount models.ComputeAllocation) {
+	slog.Info("Received compute allocation update event", "account", computeAccount)
 }
